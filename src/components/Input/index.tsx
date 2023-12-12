d14f9ce3c3ad1e04a1bb049f1ee9a692efe10ceb
MichalisG <michalis3489@gmail.com>
2023-12-12 19:31:15 +0200

    feat: input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,50 @@
+import type { FC } from 'react';
+
+interface InputProps {
+  name?: string;
+  label?: string;
+  id?: string;
+  type?: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: any;
+  error?: boolean;
+  errorMessage?: string;
+}
+
+const Input: FC<InputProps> = (props: InputProps) => {
+  const {
+    type = 'text',
+    placeholder = '',
+    label,
+    value,
+    onChange,
+    error,
+    errorMessage,
+    ...rest
+  } = props;
+
+  const errorClass = error ? 'input-error' : ''
+
+  return (
+    <label className="form-control w-full max-w-xs">
+      <div className="label p-0">
+        <span className="label-text">{label}</span>
+      </div>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={`focus:outline-none input input-bordered w-full max-w-xs ${errorClass}`}
+        {...rest}
+      />
+      <div className="label p-0">
+        <span className="label-text-alt text-error">{error ? errorMessage : <>&nbsp;</>}</span>
+      </div>
+    </label>
+
+  );
+}
+
+export default Input;
