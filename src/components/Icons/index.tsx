ed8cb9d325f3a4f8e0accc5dbfd89f8bc0990546
MichalisG <michalis3489@gmail.com>
2023-12-13 12:45:12 +0200

    feat: create Icon component

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,34 @@
+import Plus from "./Plus";
+
+export default class Icons {
+  static Search() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-5 h-5 text-rose-500"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+        />
+      </svg>
+    );
+  }
+  static Close() {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x cursor-pointer hover:text-teal-400 rounded-full w-4 h-4 ml-2">
+        <line x1="18" y1="6" x2="6" y2="18"></line>
+        <line x1="6" y1="6" x2="18" y2="18"></line>
+      </svg>
+    );
+  }
+
+  static Add() {
+    return <Plus />
+  }
+}
