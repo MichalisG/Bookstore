d9a1236ef84cb0d82a803372a2b6844e255b64bf
MichalisG <michalis3489@gmail.com>
2023-12-14 16:26:16 +0200

    fix: scroll to top when changing page

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Breadcrumbs from '../components/Breadcrumbs';
+import ScrollToTop from '../components/ScrollToTop';
 interface LayoutProps {}
 
 const Layout: FC<LayoutProps> = () => {
   return (
-    <div className="flex flex-col h-screen justify-between">
+    <div className="flex flex-col min-h-screen h-full justify-between bg-base-200">
+      <ScrollToTop/>
       <Header />
       <main className="mb-auto">
         <Breadcrumbs/>
