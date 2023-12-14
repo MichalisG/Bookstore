78cf0716a8d23f6499007db3fef6a26dc3f59e93
MichalisG <michalis3489@gmail.com>
2023-12-14 18:00:21 +0200

    fix: make logo a link to home page

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,11 +1,12 @@
 import type { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 interface LogoProps {}
 
 const Logo: FC<LogoProps> = () => {
   return (
     <div className="flex-1">
-      <a className="btn btn-ghost text-xl">Bookstore</a>
+      <Link to='/' className="btn btn-ghost text-xl">Bookstore</Link>
     </div>
   );
 }
